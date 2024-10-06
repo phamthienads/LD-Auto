98799166177bc8e3be6698fbe68c5c14d76ead4a
Neil <phamthien.ads@icloud.com>
2024-10-06 08:57:39 +0700

    modified:   index.py 	deleted:    platform-tools/AdbWinApi.dll 	deleted:    platform-tools/AdbWinUsbApi.dll 	deleted:    platform-tools/NOTICE.txt 	deleted:    platform-tools/adb.exe 	deleted:    platform-tools/etc1tool.exe 	deleted:    platform-tools/fastboot.exe 	deleted:    platform-tools/hprof-conv.exe 	deleted:    platform-tools/libwinpthread-1.dll 	deleted:    platform-tools/make_f2fs.exe 	deleted:    platform-tools/make_f2fs_casefold.exe 	deleted:    platform-tools/mke2fs.conf 	deleted:    platform-tools/mke2fs.exe 	deleted:    platform-tools/source.properties 	deleted:    platform-tools/sqlite3.exe

--- index.py
+++ index.py
@@ -3,12 +3,9 @@ import random
 import string
 import time
 
-# Đường dẫn đầy đủ đến ADB trong thư mục platform-tools
-ADB_PATH = "platform-tools/adb.exe"  # Cập nhật đường dẫn ADB tại đây
-
 # Hàm thực hiện lệnh adb thông qua cmd
 def adb_command(command):
-    full_command = f'cmd /c "{ADB_PATH} {command}"'
+    full_command = f'adb {command}'
     process = subprocess.Popen(full_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
